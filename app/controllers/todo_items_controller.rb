bec84d219dab91ce9433b43cdb95d829fcb24d6e
Johnson Lai <jlwhoo7@gmail.com>
2017-02-23 15:22:00 +0000

    added complete button and db

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
     
     before_action :set_todo_list
+    before_action :set_todo_item, except: [:create]
     
     def create
        @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -18,11 +19,20 @@ class TodoItemsController < ApplicationController
         redirect_to @todo_list
     end
     
+    def complete
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to @todo_list, notice: "To do item completed"
+    end
+    
     private
     def set_todo_list
         @todo_list = TodoList.find(params[:todo_list_id])
     end
-
+    
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+    
     def todo_item_params
        params[:todo_item].permit(:content) 
     end
